0f0ab2a150fae70dd2e8030c91247ba88396c17f
Jonas <josl@dhi-gras.com>
2017-01-27 14:54:00 +0100

    use setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
-
-setup(name='atmospheric_correction',
-      version='0.1',
-      description='atmospheric_correction toolkit',
-      author='Jonas Solvsteen',
-      author_email='josl@dhi-gras.com',
-      url='https://www.dhi-gras.com',
-      packages=['atmospheric_correction'],
-      package_dir={'atmospheric_correction': ''},
-      package_data={'atmospheric_correction': ['dependency\sensorResponseCurves\*.txt', 'dependency\sixsV1.1']}
-      )
+setup(
+    name='atmospheric_correction',
+    version='0.1',
+    description='Atmospheric Correction using 6S',
+    author='Jonas Solvsteen',
+    author_email='josl@dhi-gras.com',
+    packages=['atmospheric_correction'],
+    package_dir={'atmospheric_correction': ''},
+    include_package_data=True,
+    install_requires=['gdal_utils'],
+    dependency_links=['https://github.com/DHI-GRAS/gdal_utils.git'])
