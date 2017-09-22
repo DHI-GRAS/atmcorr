b803d2100dab65726a67f963ec1d3c9f5648a619
Jonas Sølvsteen <j08lue@gmail.com>
2017-09-22 11:39:24 +0200

    new faster implementation of tiling

--- atmospheric_correction/tiling.py
+++ atmospheric_correction/tiling.py
@@ -1,12 +1,10 @@
 from __future__ import division
 
 import numpy as np
-
-import rasterio.windows
-import rasterio.warp
+import pyproj
 
 
-def get_tiled_windows(height, width, tilesize):
+def get_tile_corners(height, width, tilesize):
     """Tile image with width and height into windows of size tilesize
 
     Paramters
@@ -23,110 +21,93 @@ def get_tiled_windows(height, width, tilesize):
     """
     nx = int(width / tilesize + 0.5)
     ny = int(height / tilesize + 0.5)
-    windows = np.empty((ny, nx), dtype=object)
-    for j in range(ny):
-        for i in range(nx):
-            col_offset = i * tilesize
-            row_offset = j * tilesize
-            w = rasterio.windows.Window(col_offset, tilesize, row_offset, tilesize)
-            if j == (ny == 1) or i == (nx - 1):
-                w = rasterio.windows.crop(w, height, width)
-            windows[j, i] = w
-    return windows
+
+    jj = np.arange(ny)
+    ii = np.arange(nx)
+    jmesh, imesh = np.meshgrid(jj, ii, indexing='ij')
+    assert jmesh.shape == (ny, nx)
+
+    # tile grid dims, 4 corners, (x, y)
+    corners = np.zeros((2, 4, ny, nx))
+
+    bottom = jmesh * tilesize
+    left = imesh * tilesize
+    top = np.minimum((bottom + tilesize - 1), ny)
+    right = np.minimum((left + tilesize - 1), nx)
+    # now create four corner points
+    corners[:, 0, ...] = np.stack((left, bottom), axis=0)
+    corners[:, 1, ...] = np.stack((left, top), axis=0)
+    corners[:, 2, ...] = np.stack((right, top), axis=0)
+    corners[:, 3, ...] = np.stack((right, bottom), axis=0)
+    return corners
 
 
-def _transform_bounds(w, src_transform, src_crs, dst_crs):
-    src_bounds = rasterio.windows.bounds(w, src_transform)
-    return rasterio.warp.transform_bounds(src_crs, dst_crs, *src_bounds)
-
-
-def get_transformed_bounds(windows, src_transform, src_crs, dst_crs):
-    """Get bounds for a sequence of windows in dst_crs
+def transform_corners(corners, src_transform, src_crs, dst_crs={'init': 'epsg:4326'}):
+    """Transform corners from array indices to dst_crs coordinates
 
     Parameters
     ----------
-    windows : ndarray of rasterio.windows.Window
-        windows
-    src_transform : Affine
+    corners : ndarray shape(2, ...) dtype(int)
+        x,y pairs for N corners
+    src_transform : affine.Affine
         source image transform
-    src_crs, dst_crs : rasterio.crs.CRS or similar
-        source and destination coordinate
-        reference systems
+    src_crs : dict or rasterio.crs.CRS
+        source coordinate reference system
+    dst_crs : dict or rasterio.crs.CRS
+        destination coordinate reference system
+        default: WGS84 (lon, lat)
+
+    Returns
+    -------
+    ndarray, ndarray
+        projected coordinates
     """
-    newshape = (4, ) + windows.shape
-    bounds = np.zeros(newshape, int)
-    winiter = windows.flat
-    for _ in range(windows.size):
-        w = next(winiter)
-        j, i = winiter.coords
-        bounds[:, j, i] = _transform_bounds(w, src_transform, src_crs, dst_crs)
-    return bounds
+    src_proj = pyproj.Proj(src_crs)
+    dst_proj = pyproj.Proj(dst_crs)
+    xs, ys = src_transform * corners
+    xout, yout = pyproj.transform(src_proj, dst_proj, xs, ys)
+    return xout, yout
 
 
-def getTileExtents(width, height, transform, tileSize):
-    """ Split the image into square tiles of tileSize pixels
+def corners_to_extents(xs, ys):
+    """Convert arrays of corner coordinates to an extent record array
 
     Parameters
     ----------
-    width, height : int
-        nx, ny shape of image
-    transform : affine.Affine
+    xs, ys : ndarray shape(N, ...)
+        x and y coordinates
+
+    Returns
+    -------
+    np.recarray shape(...)
+        xmin, xmax, ymin, ymax
+    """
+    extent_rec = np.core.records.fromarrays(
+            [xs.min(axis=0), xs.max(axis=0), ys.min(axis=0), ys.max(axis=0)],
+            names=['xmin', 'xmax', 'ymin', 'ymax'])
+    return extent_rec
+
+
+def get_tile_extents(height, width, tilesize, src_transform, src_crs):
+    """For an image of height, width get extents for tiles of size tilesize
+
+    Parameters
+    ----------
+    height, width : int
+        image shape
+    tilesize : int
+        symmetric tile size
+    src_transform : affine.Affine
         image transform
-    tileSize : int
-        number of pixels in each square tile
+    src_crs : dict or rasterio.crs.CRS
+        source coordinate reference system
 
     Returns
     -------
-    extents : ([minX, maxY, maxX, minY])
-        extents of each tile in the projected units.
+    np.recarray shape(...)
+        xmin, xmax, ymin, ymax
     """
-    gt = transform.to_gdal()
-
-    nx = int(width // tileSize) + 1
-    ny = int(height // tileSize) + 1
-    rows = [int(i * tileSize) for i in range(ny)]
-    cols = [int(i * tileSize) for i in range(nx)]
-    rows.append(height)
-    cols.append(width)
-
-    nrows = len(rows)
-    ncols = len(cols)
-    tileExtents = np.zeros((nrows, ncols, 4))
-    for j in range(1, nrows):
-        for i in range(1, ncols):
-            ext = getExtent(gt, cols[i]-1, rows[j]-1, cols[i-1], rows[j-1])
-            tileExtents[j-1, i, :] = [ext[0][0], ext[0][1], ext[2][0], ext[2][1]]
-
-    return tileExtents
-
-
-def getExtent(gt, endCol, endRow, startCol=0, startRow=0):
-    """Get list of corner coordinates from geotransform
-
-    Parameters
-    ----------
-    gt : sequence of float
-        GDAL geotransform
-    endCol : int
-        ending column of the subset relative to gt origin
-    endRow : int
-        ending row of the subset relative to gt origin
-    startCol : int
-        starting column of the subset relative to gt origin
-    startRow : int
-        starting row of the subset relative to gt origin
-
-    Returns
-    -------
-    list of (float, float) : x,y coordinates of each corner
-    """
-    ext = []
-    xarr = [startCol, endCol]
-    yarr = [startRow, endRow]
-    for px in xarr:
-        for py in yarr:
-            x = gt[0] + (px * gt[1]) + (py * gt[2])
-            y = gt[3] + (px * gt[4]) + (py * gt[5])
-            ext.append([x, y])
-        yarr.reverse()
-    return ext
+    corners = get_tile_corners(height, width, tilesize)
+    lon, lat = transform_corners(corners, src_transform, src_crs)
+    extents_rec = corners_to_extents(lon, lat)
+    return extents_rec
--- tests/test_tiling.py
+++ tests/test_tiling.py
@@ -3,22 +3,31 @@ import affine
 from atmospheric_correction import tiling
 
 
-def test_get_tiled_windows():
-    windows = tiling.get_tiled_windows(height=1000, width=2000, tilesize=10)
-    assert windows.shape == (100, 200)
+def test_get_tile_corners():
+    corners = tiling.get_tile_corners(height=1000, width=2000, tilesize=100)
+    assert corners.shape == (2, 4, 10, 20)
 
 
-def test_get_tiled_windows_round():
-    windows = tiling.get_tiled_windows(height=999, width=1999, tilesize=10)
-    assert windows.shape == (100, 200)
+def test_get_tile_corners_round():
+    corners = tiling.get_tile_corners(height=999, width=1999, tilesize=100)
+    assert corners.shape == (2, 4, 10, 20)
 
 
-def test_get_transformed_bounds():
-    windows = tiling.get_tiled_windows(height=1000, width=2000, tilesize=10)
-    bb = tiling.get_transformed_bounds(
-            windows,
+def test_transform_corners():
+    corners = tiling.get_tile_corners(height=1000, width=2000, tilesize=100)
+    lon, lat = tiling.transform_corners(
+            corners,
             src_transform=affine.Affine(10, 0, 36000, 0, 10, 18000),
-            src_crs={'init': 'epsg:4032'},
-            dst_crs={'init': 'epsg:4326'})
+            src_crs={'init': 'epsg:4032'})
+    assert lon.shape == (4, 10, 20)
 
-    assert bb.shape == (4, ) + windows.shape
+
+def test_get_tile_extents():
+    extent_rec = tiling.get_tile_extents(
+            height=1000,
+            width=2000,
+            tilesize=100,
+            src_transform=affine.Affine(10, 0, 36000, 0, 10, 18000),
+            src_crs={'init': 'epsg:4032'})
+    assert extent_rec.shape == (10, 20)
+    assert 'xmin' in extent_rec.dtype.names
