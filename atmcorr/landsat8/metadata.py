e8e10765d07d3081255926a0256edede1c6e727d
Jonas Sølvsteen <j08lue@gmail.com>
2017-11-09 21:44:46 +0100

    use new satmeta.l8

--- atmcorr/landsat8/metadata.py
+++ atmcorr/landsat8/metadata.py
@@ -1,31 +1,7 @@
-import re
-
-import dateutil.parser
+import satmeta.l8.meta as l8meta
 
 
 def get_date(mtdFile):
-    """
-    DATE_ACQUIRED = 2017-09-09
-    SCENE_CENTER_TIME = "10:32:22.4278750Z"
-    """
-    date_acquired_pattern = re.compile('DATE_ACQUIRED\s=\s([\d-]*)')
-    scene_center_time_pattern = re.compile('SCENE_CENTER_TIME\s=\s"([\d:\.]*Z)"')
-
-    datestr = None
-    timestr = None
-    with open(mtdFile, 'r') as metadata:
-        for line in metadata:
-            match = date_acquired_pattern.search(line)
-            if match is not None:
-                datestr = match.group(1)
-            match = scene_center_time_pattern.search(line)
-            if match is not None:
-                timestr = match.group(1)
-
-    if datestr is None or timestr is None:
-        raise ValueError(
-                'Unable to get datestr ({}) and/or timestr ({}) from MTL file \'{}\'.'
-                .format(datestr, timestr, mtdFile))
-
-    fullstr = datestr + 'T' + timestr
-    return dateutil.parser.parse(fullstr)
+    with open(mtdFile) as fin:
+        metadata = l8meta.parse_metadata(fin)
+    return metadata['sensing_time']
