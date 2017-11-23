d5d2c05816e65320017ef4b212521760fc599acf
Jonas Sølvsteen <j08lue@gmail.com>
2017-11-23 12:49:24 +0100

    bump version

--- atmcorr/scripts/atmcorr.py
+++ atmcorr/scripts/atmcorr.py
@@ -1,39 +1,30 @@
-import logging
 import click
 import yamlconfig.click_option
-
-logger = logging.getLogger(__name__)
 
 
 @click.command()
 @yamlconfig.click_option.yaml_config_option(
         keys=[
-            'sensor', 'dnFile', 'mtdFile',
-            'method', 'atm', 'aeroProfile', 'tileSizePixels',
+            'sensor', 'dnFile', 'mtdFile', 'band_ids',
+            'method', 'atm', 'aeroProfile', 'tileSize',
             'outfile',
             ('adjCorr', False),
             ('mtdFile_tile', None),
-            ('nprocs', None),
-            ('band_ids', None),
             ('use_modis', False),
-            ('earthdata_credentials', {})],
+            ('earthdata_credentials', None)],
         allow_missing=False,
         parse_kwargs=dict(
             join_rootdir=True),
         help='Atmospheric correction config file')
-def cli(dnFile, outfile, band_ids, **config):
+def cli(dnFile, outfile, **config):
     """Run atmospheric correction"""
     from atmcorr.logs import set_cli_logger
     set_cli_logger(level='INFO')
     from atmcorr.processing import main
     import rasterio
-    readkw = {}
     with rasterio.open(dnFile) as src:
-        if band_ids:
-            readkw.update(indexes=list(band_ids))
-        data = src.read(**readkw)
+        data = src.read()
         profile = src.profile
-    config.update(band_ids=band_ids)
     outdata, outprofile = main(data=data, profile=profile, **config)
     with rasterio.open(outfile, 'w', **outprofile) as dst:
         dst.write(outdata)
