a877eb31932c7ee0ada6aba1b66a07424060f73e
Jonas Sølvsteen <j08lue@gmail.com>
2017-11-29 09:08:33 +0100

    move viewing geometry

--- atmcorr/viewing_geometry/__init__.py
+++ atmcorr/viewing_geometry/__init__.py
@@ -1,4 +1,5 @@
-from atmcorr.sensors import sensor_is, sensor_is_any
+from atmcorr.sensors import sensor_is
+from atmcorr.sensors import sensor_is_any
 
 REQUIRED_KEYS = {'sun_zenith', 'sun_azimuth', 'sensor_zenith', 'sensor_azimuth', 'month', 'day'}
 
@@ -12,16 +13,16 @@ def _check_gdict(d):
 
 def get_geometry(sensor, mtdFile, **kwargs):
     if sensor_is_any(sensor, 'WV', 'WV_4band'):
-        from atmcorr import worldview
-        gdict = worldview.geometry.get_geometry(mtdFile)
+        from . import worldview
+        gdict = worldview.get_geometry(mtdFile)
     elif sensor_is(sensor, 'PHR'):
-        from atmcorr import pleiades
-        gdict = pleiades.geometry.get_geometry(mtdFile)
+        from . import pleiades
+        gdict = pleiades.get_geometry(mtdFile)
     elif sensor_is(sensor, 'L7L8'):
-        from atmcorr import landsat8
-        gdict = landsat8.geometry.get_geometry(mtdFile)
+        from . import landsat8
+        gdict = landsat8.get_geometry(mtdFile)
     elif sensor_is(sensor, 'S2'):
-        from atmcorr import sentinel2
-        gdict = sentinel2.geometry.get_geometry(mtdFile, **kwargs)
+        from . import sentinel2
+        gdict = sentinel2.get_geometry(mtdFile, **kwargs)
     _check_gdict(gdict)
     return gdict
