d8f920b98bace5888f9df27f9a61cb132e74a3fc
Jonas Sølvsteen <j08lue@gmail.com>
2017-12-04 10:29:43 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='atmcorr',
-    version='0.13',
+    version='0.14',
     description='Atmospheric Correction using 6S',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
@@ -13,9 +13,5 @@ setup(
     """,
     install_requires=[
         'dg_calibration',
-        'sensor_response_curves>=0.4',
-        'satmeta'],
-    dependency_links=[
-        'https://github.com/DHI-GRAS/dg-calibration/archive/master.zip',
-        'https://github.com/DHI-GRAS/sensor_response_curves/archive/v0.4.tar.gz#egg=sensor_response_curves-0.4',
-        'https://github.com/DHI-GRAS/satmeta/archive/v0.11.tar.gz#egg=satmeta-0.11'])
+        'sensor_response_curves',
+        'satmeta'])
