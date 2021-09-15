84ef50261ad015b1a8476694e86995c909310725
panakouris <ppkouris@gmail.com>
2021-09-15 15:43:29 +0200

    revert to using pyproj

--- atmcorr/tiling.py
+++ atmcorr/tiling.py
@@ -1,7 +1,7 @@
 from __future__ import division
 
 import affine
-import rasterio.warp
+import pyproj
 import numpy as np
 
 
@@ -81,8 +81,10 @@ def _transform_corners(corners, src_crs, dst_crs):
     ndarray, ndarray
         projected coordinates
     """
+    src_proj = pyproj.Proj(src_crs)
+    dst_proj = pyproj.Proj(dst_crs)
     xs, ys = corners
-    xout, yout = rasterio.warp.transform(src_crs, dst_crs, xs, ys)
+    xout, yout = pyproj.transform(src_proj, dst_proj, xs, ys)
     return xout, yout
 
 
@@ -111,7 +113,7 @@ def _corners_to_extents(xs, ys):
     return extent_rec
 
 
-def get_projected_extents(transform, height, width, src_crs, dst_crs='epsg:4326'):
+def get_projected_extents(transform, height, width, src_crs, dst_crs={'init': 'epsg:4326'}):
     """Get extents of pixels in WGS84 or other projection
 
     Parameters
@@ -136,7 +138,7 @@ def get_projected_extents(transform, height, width, src_crs, dst_crs='epsg:4326'
     return _corners_to_extents(xproj, yproj)
 
 
-def bounds_to_projected_extents(left, bottom, right, top, src_crs, dst_crs='epsg:4326'):
+def bounds_to_projected_extents(left, bottom, right, top, src_crs, dst_crs={'init': 'epsg:4326'}):
     """Get extents record array from bounds
 
     Parameters
@@ -151,13 +153,15 @@ def bounds_to_projected_extents(left, bottom, right, top, src_crs, dst_crs='epsg
     np.recarray shape (1, 1)
         with names xmin, xmax, ymin, ymax
     """
+    src_proj = pyproj.Proj(src_crs)
+    dst_proj = pyproj.Proj(dst_crs)
     xs = np.array([left, left, right, right])
     ys = np.array([bottom, top, top, bottom])
-    xproj, yproj = rasterio.warp.transform(src_crs, dst_crs, xs, ys)
+    xproj, yproj = pyproj.transform(src_proj, dst_proj, xs, ys)
     return _corners_to_extents(xproj, yproj)[np.newaxis, np.newaxis]
 
 
-def get_projected_image_extent(transform, height, width, src_crs, dst_crs='epsg:4326'):
+def get_projected_image_extent(transform, height, width, src_crs, dst_crs={'init': 'epsg:4326'}):
     """Get extents of a whole image in WGS84 or other projection
 
     Parameters
